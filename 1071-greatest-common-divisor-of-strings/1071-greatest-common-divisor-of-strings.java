eff43ec06a8724f7a66f7a163c8a7c16312e032c
Krishna Kant Pathak <krishnakant9894@gmail.com>
2026-07-01 06:51:32 +0530

    Time: 7 ms (6.94%), Space: 43.2 MB (97.15%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -3,19 +3,21 @@ class Solution {
         String min = str1.length() <= str2.length() ? str1 : str2;
         String max = str1.length() <= str2.length() ? str2 : str1;
         StringBuilder sb = new StringBuilder();
-        boolean flag = false;
-        for(int i=min.length()-1 ; i>=0 ; i--) {
-            if (i!=0 && max.length()%(i+1) !=0 || min.length()%(i+1) != 0) continue;
-            for(int j=0; j<=i; j++) {
+        for (int i = min.length() - 1; i >= 0; i--) {
+            if (i != 0 && max.length() % (i + 1) != 0 || min.length() % (i + 1) != 0)
+                continue;
+            for (int j = 0; j <= i; j++) {
                 sb.append(min.charAt(j));
             }
-            for(int k=0; k<max.length(); k++) {
-                if(max.charAt(k) != sb.charAt(k%sb.length()) || min.charAt(k%min.length()) != sb.charAt(k%sb.length())) break;
-                else if( k==max.length()-1) flag=true;
+            for (int k = 0; k < max.length(); k++) {
+                if (max.charAt(k) != sb.charAt(k % sb.length())
+                        || min.charAt(k % min.length()) != sb.charAt(k % sb.length()))
+                    break;
+                else if (k == max.length() - 1)
+                    return sb.toString();
             }
-            if (flag) break; 
             sb.setLength(0);
         }
-        return flag ? sb.toString() : "";
+        return "";
     }
 }
