e4daf26ff3e4cd6e8391960cf4d4500752b07336
Krishna Kant Pathak <krishnakant9894@gmail.com>
2026-07-01 06:36:24 +0530

    Time: 7 ms (6.94%), Space: 43 MB (99.24%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        String min = str1.length() <= str2.length() ? str1 : str2;
+        String max = str1.length() <= str2.length() ? str2 : str1;
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        for(int i=min.length()-1 ; i>=0 ; i--) {
+            if (i!=0 && max.length()%(i+1) !=0 || min.length()%(i+1) != 0) continue;
+            for(int j=0; j<=i; j++) {
+                sb.append(min.charAt(j));
+            }
+            for(int k=0; k<max.length(); k++) {
+                if(max.charAt(k) != sb.charAt(k%sb.length()) || min.charAt(k%min.length()) != sb.charAt(k%sb.length())) break;
+                else if( k==max.length()-1) flag=true;
+            }
+            if (flag) break; 
+            sb.setLength(0);
+        }
+        return flag ? sb.toString() : "";
+    }
+}
